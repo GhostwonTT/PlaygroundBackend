5132b3dfbfb42ed16d997e07472b7444f51decb2
Jason Wu <jaaasonwu@gmail.com>
2017-09-17 22:22:23 +1000

    Separate manuals by section

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,10 @@ router.get('/manuals/:filename', function (req, res, next) {
     res.sendFile(path.resolve(__dirname + '/../public/manuals/' + filename + '.pdf'));
 });
 
+router.get('/manual_sec/:filename/:id', function (req, res, next) {
+    var filename = req.params.filename;
+    var id = req.params.id;
+    res.sendFile(path.resolve(__dirname + '/../public/manuals/' + filename + '/' + id + '.pdf'));
+})
+
 module.exports = router;
