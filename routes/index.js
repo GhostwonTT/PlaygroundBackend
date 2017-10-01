cb26f3c1c880b81081fa156fa3d16df8b9c0b61f
Jason Wu <jaaasonwu@gmail.com>
2017-10-01 23:07:44 +1100

    Change the endpoint for manual chapter

--- routes/index.js
+++ routes/index.js
@@ -7,15 +7,26 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+router.get('/manuals/info', function (req, res, next) {
+    res.send(buildInfoObject());
+})
+
 router.get('/manuals/:filename', function (req, res, next) {
     var filename = req.params.filename;
     res.sendFile(path.resolve(__dirname + '/../public/manuals/' + filename + '.pdf'));
 });
 
-router.get('/manual_sec/:filename/:id', function (req, res, next) {
+router.get('/manuals/:filename/:id', function (req, res, next) {
     var filename = req.params.filename;
     var id = req.params.id;
     res.sendFile(path.resolve(__dirname + '/../public/manuals/' + filename + '/' + id + '.pdf'));
 })
+
+function buildInfoObject() {
+    var info = {};
+    info['Playground Starter'] = {'chapters' : ['Introduction', 'Welcome']};
+    return JSON.stringify(info);
+}
 
 module.exports = router;
