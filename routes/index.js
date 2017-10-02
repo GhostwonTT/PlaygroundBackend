72a8bcc311c58a6199e8b96d0693dabce75e44fb
Jason Wu <jaaasonwu@gmail.com>
2017-10-02 16:52:36 +1100

    Rename the loose parts manual

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
 
 router.get('/manuals/info', function (req, res, next) {
     res.send(buildInfoObject());
-})
+});
 
 router.get('/manuals/:filename', function (req, res, next) {
     var filename = req.params.filename;
@@ -21,11 +21,46 @@ router.get('/manuals/:filename/:id', function (req, res, next) {
     var filename = req.params.filename;
     var id = req.params.id;
     res.sendFile(path.resolve(__dirname + '/../public/manuals/' + filename + '/' + id + '.pdf'));
-})
+});
+
+router.get('/manuals/info', function (req, res, next) {
+    res.send(buildInfoObject());
+});
 
 function buildInfoObject() {
     var info = {};
-    info['Playground Starter'] = {'chapters' : ['Introduction', 'Welcome']};
+    info['Playground Starter'] = {'id' : '0',
+        'chapters' : ['Welcome', 'Let\'s Build A Playground', 'Site Plan', 'Elements', 'Join Us']};
+
+    info['Builder\'s Handbook'] = {'id' : '1',
+        'chapters' : ['Introduction', 'Step 1: Listen', 'Step 2: Plan',
+            'Step 3: Design', 'Step 4: Build', 'Step 5: Maintain', 'Appendices']};
+
+    info['Safety Handbook'] = {'id' : '2',
+        'chapters' : ['Managing', 'Hazards', 'Heights + Fall Zone']};
+
+    info['Loose Parts Manual'] = {'id' : '3',
+        'chapters' : ['Welcome', 'Benefits Of Loose Parts',
+            'Step 1: Assess Your Environment',
+            'Step 2: Gather Loose Parts Materials',
+            'Step 3: Storage And Maintenance', 'Step 4: Train Staff',
+            'Going Deeper', 'Thank You', 'Further Reading & Resources']};
+
+    info['Inclusive Design Manual'] = {'id' : '4',
+        'chapters' : ['Intro', 'Listen', 'Tips And Strategies', 'Design']};
+
+    info['Cut & Paste'] = {'id' : '5',
+        'chapters' : ['Cut & Paste Playground Designer']};
+
+    info['Teacher Training'] = {'id' : '6',
+        'chapters' : ['Welcome', 'The Transforming Power Of Play',
+            'Why Do Children Play', 'What Does Play Look Like',
+            'Obstacles To Play', 'Rote Based Learning Vs. Play Based Learning',
+            'Be A Play Advocate', 'Keep Learning', 'References']};
+
+    info['Case for Play'] = {'id' : '7',
+        'chapters' : ['Executive Summary', 'Introduction', 'The Issue',
+            'Potential Of Play Interventions', 'Call To Action', 'References']};
     return JSON.stringify(info);
 }
 
